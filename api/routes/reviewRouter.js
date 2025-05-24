19ecb49b3a3cae6f726210c59fcf9f4df6fd7223
Mostafijur-1 <mostafij155546@gmail.com>
2025-05-24 22:35:36 +0600

    socket,review

--- api/routes/reviewRouter.js
+++ api/routes/reviewRouter.js
@@ -1,25 +1,15 @@
 import express from "express";
+import {
+  createReview,
+  getReviewsForUser,
+  getFlaggedUsers,
+} from "../controllers/review.js";
+import { admin, protect } from "../middleware/auth.js";
 
 const router = express.Router();
-import {
-  getAllReviews,
-  getReviewById,
-  createReview,
-  updateReview,
-  deleteReview,
-} from "../controllers/reviewController.js";
 
-import { protect, admin } from "../middleware/auth.js";
-
-// GET all reviews
-router.get("/", protect, getAllReviews);
-// GET review by ID
-router.get("/:id", protect, getReviewById);
-// POST create a new review
 router.post("/", protect, createReview);
-// PUT update a review
-router.put("/:id", protect, updateReview);
-// DELETE a review
-router.delete("/:id", protect, admin, deleteReview);
+router.get("/user/:userId", getReviewsForUser);
+router.get("/admin/flagged", admin, getFlaggedUsers);
 
 export default router;
